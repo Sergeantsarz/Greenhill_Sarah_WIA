943190cb5a12037121e6cfe152a8d68d9fde5c46
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-08 13:24:34 -0500

    Properly crediting the video and changing time

--- Greenhill_Sarah_WIA_Presentation/js/main.js
+++ Greenhill_Sarah_WIA_Presentation/js/main.js
@@ -2,7 +2,7 @@
 //WIA - December 2015 - Presentation
 
 $(document).ready(function(){
-  $('#wrapper').tubular({videoId: 't0I4mTEdAf8'});
-  console.log('This video is credited at the following link: https://www.youtube.com/watch?v=t0I4mTEdAf8');
+  $('#wrapper').tubular({videoId: '6tmbeLTHC_0'});
+  console.log('The video on this site is credited to NASA at the following link source: https://www.youtube.com/watch?v=6tmbeLTHC_0');
   
 });
