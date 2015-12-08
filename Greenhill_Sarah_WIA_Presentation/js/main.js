16fd0e0a70d1ed64f3b9ec42a859a37e7def5190
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-07 19:13:34 -0500

    Broke wrapper, fix by removing

--- Greenhill_Sarah_WIA_Presentation/js/main.js
+++ Greenhill_Sarah_WIA_Presentation/js/main.js
@@ -3,9 +3,6 @@
 
 $(document).ready(function(){
   $('#wrapper').tubular({videoId: '6v2L2UGZJAM'});
-  $('#wrapper').tubular({playButtonClass: 'tubular-play'});
-  $('#wrapper').tubular({pauseButtonClass: 'tubular-pause'});
-  $('#wrapper').tubular({muteButtonClass: 'tubular-mute'});
   
   
 });
