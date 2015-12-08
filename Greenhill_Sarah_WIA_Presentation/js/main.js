c0642a595c6dd44c1c7a84a2f66148d525090f45
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-08 13:21:52 -0500

    Changing settings and video

--- Greenhill_Sarah_WIA_Presentation/js/main.js
+++ Greenhill_Sarah_WIA_Presentation/js/main.js
@@ -2,6 +2,7 @@
 //WIA - December 2015 - Presentation
 
 $(document).ready(function(){
-  $('#wrapper').tubular({videoId: '6tmbeLTHC_0'});
+  $('#wrapper').tubular({videoId: 't0I4mTEdAf8'});
+  console.log('This video is credited at the following link: https://www.youtube.com/watch?v=t0I4mTEdAf8');
   
 });
