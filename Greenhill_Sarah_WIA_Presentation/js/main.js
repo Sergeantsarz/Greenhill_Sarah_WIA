a9ade88ad09f2d2aa085eec00f03e1a1680b2bf3
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-08 13:09:14 -0500

    Changing the video back

--- Greenhill_Sarah_WIA_Presentation/js/main.js
+++ Greenhill_Sarah_WIA_Presentation/js/main.js
@@ -2,6 +2,6 @@
 //WIA - December 2015 - Presentation
 
 $(document).ready(function(){
-  $('#wrapper').tubular({videoId: '6tmbeLTHC_0&feature=youtu.be&t=55'});
+  $('#wrapper').tubular({videoId: '6tmbeLTHC_0'});
   
 });
