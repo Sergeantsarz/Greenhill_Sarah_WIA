59421127e72f666037ac2aaa7cc4ac117d541e4b
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-08 14:43:00 -0500

    Added comments and trying to fade the alert

--- Greenhill_Sarah_WIA_Presentation/js/main.js
+++ Greenhill_Sarah_WIA_Presentation/js/main.js
@@ -2,7 +2,15 @@
 //WIA - December 2015 - Presentation
 
 $(document).ready(function(){
+	//Sets the YouTube video as the background
   $('#wrapper').tubular({videoId: '6tmbeLTHC_0'});
+  
+  //Animate the quote alert 
+  $(".alert button.close").click(function (e) {
+    $(this).parent().fadeOut('slow');
+});
+  
+  //Video Reference
   console.log('The video on this site is credited to NASA at the following link source: https://www.youtube.com/watch?v=6tmbeLTHC_0');
   
 });
