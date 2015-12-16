7ec9052d595cfb9ff0c48ec1841749f2b638fe38
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-16 17:37:12 -0500

    changing alignment

--- Greenhill_Sarah_WIA_The_Project/js/main.js
+++ Greenhill_Sarah_WIA_The_Project/js/main.js
@@ -56,6 +56,7 @@ window.onload = function(){
 	
 			ctx.font = "20pt Love Ya Like A Sister";
 			ctx.fillStyle = '#ffffff';
+			ctx.textAlign="center";
 			ctx.fillText("About The Zombie Outbreak", 620, 80);	
 		}
 	}
