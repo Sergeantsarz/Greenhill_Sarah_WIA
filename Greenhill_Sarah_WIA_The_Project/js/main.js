51cdd985729bd325f914a58add9258e2ba7e8919
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-16 14:09:38 -0500

    Adding more things

--- Greenhill_Sarah_WIA_The_Project/js/main.js
+++ Greenhill_Sarah_WIA_The_Project/js/main.js
@@ -1,1 +1,59 @@
 console.log('JavaScript is linked.');
+
+//Modernizr
+
+window.onload = function(){
+if(Modernizr.canvas){
+	//Canvas is supported
+	
+	var theCanvas = document.getElementById("supported1");
+	var ctx = theCanvas.getContext("2d");
+	
+	//Let User Know if Canvas Works
+	ctx.font = "12pt Helvetica";
+	ctx.fillText("Canvas is supported in this browser!", 20, 60);
+	
+	
+	}else {
+		//Canvas is not supported
+		//Polyfill would go here
+		ctx.fillText("Canvas is not supported in this browser!", 20, 60);
+		
+		}
+
+console.log(Modernizr);
+
+//Modernizr Tests
+
+Modernizr.load({
+	test: Modernizr,
+	yep: "js/success.js",
+	nope: "js/fail.js",
+	complete: function(){
+		//Runs after JS file loads
+		
+		console.log("Testing functionality complete!");
+		}
+	});
+
+Modernizr.load({
+	test: Modernizr.canvas,
+	yep: "js/success.js",
+	nope: "js/flashcanvas.js",
+	
+	});	
+
+//Canvas 
+
+var content = document.getElementById("content1");
+
+if(content){
+
+		var ctx = text.getContext("2d");
+	if(ctx){
+	
+	ctx.font = "12pt Love Ya Like A Sister";
+	ctx.fillText("About Zombies", 10, 80);	
+		
+	}
+}
