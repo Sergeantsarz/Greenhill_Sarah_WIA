5aebbad265a11adac326c6082705d9f01d2473db
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-12-16 14:40:23 -0500

    Moving again

--- Greenhill_Sarah_WIA_The_Project/js/main.js
+++ Greenhill_Sarah_WIA_The_Project/js/main.js
@@ -54,7 +54,7 @@ window.onload = function(){
 	
 			ctx.font = "20pt Love Ya Like A Sister";
 			ctx.fillStyle = '#ffffff';
-			ctx.fillText("About Zombies", 40, 80);	
+			ctx.fillText("About Zombies", 100, 80);	
 		}
 	}
 
